435d84775156e1a86b1a5e22091821d3c9e61688
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-07-24 23:47:31 +0300

    Finished working on the files.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { movies } from "../data";
+import {movies} from "../data";
 
-function Movies() {
-  return <div>{/*{code here}*/}</div>;
+function Movies(){
+  return <div>
+    <h1>Movies Page</h1>
+    {movies.map((movie) => (
+      <div key={movie} >
+        <h3>Movie Title: {movie.title}</h3>
+        <h3>Time: {movie.time} minutes</h3>
+        <ul key={{movie} + movie.genres}> Genres:
+          {movie.genres.map((genre) => (
+            <li key={{movie} + {genre}}>
+              {genre}
+            </li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </div>;
 }
 
 export default Movies;
